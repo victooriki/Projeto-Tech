09f01510a27fb7b3140d5101657e9c305b165f5f
Victor Iki <victooriki@gmail.com>
2023-05-05 09:06:02 -0300

    Adicionando widgets
    Clientes, funcionários e produtos

--- app/Controllers/Inicio.php
+++ app/Controllers/Inicio.php
@@ -2,14 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Models\ClienteModel;
+use App\Models\FuncionarioModel;
+use App\Models\ProdutoModel;
+
 use CodeIgniter\Controller;
 
 class Inicio extends Controller
 {
+    private $cliente_model;
+    private $funcionario_model;
+    private $produto_model;
+
+    function __construct()
+    {
+        $this->cliente_model     = new ClienteModel();
+        $this->funcionario_model = new FuncionarioModel();
+        $this->produto_model = new ProdutoModel();
+    }
+
     public function index()
     {
+        $total_de_clientes = count(
+            $this->cliente_model->findAll()
+        );
+
+        $total_de_funcionarios = count(
+            $this->funcionario_model->findAll()
+        );
+
+        $total_de_produtos = count(
+            $this->produto_model->findAll()
+        );
+
+
+        $data['total_de_clientes'] = $total_de_clientes;
+        $data['total_de_funcionarios'] = $total_de_funcionarios;
+        $data['total_de_produtos'] = $total_de_produtos;
+
         echo View('templates/header');
-        echo View('inicio/index');
+        echo View('inicio/index', $data);
         echo View('templates/footer');
     }
 }
--- app/Views/inicio/index.php
+++ app/Views/inicio/index.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
-    $session = session();
+$session = session();
 
-    $primeiro_nome = $session->get('primeiro_nome');
+$primeiro_nome = $session->get('primeiro_nome');
 
 ?>
 
@@ -29,8 +29,39 @@
     <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
-            
-        <!-- CONTEÚDO -->
+
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-info"><i class="fas fa-users"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Clientes</span>
+                            <span class="info-box-number"><?= $total_de_clientes ?></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-success"><i class="fas fa-users"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Funcionários</span>
+                            <span class="info-box-number"><?= $total_de_funcionarios ?></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-warning"><i class="fas fa-box-open"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Produtos</span>
+                            <span class="info-box-number"><?= $total_de_produtos ?></span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
         </div><!-- /.container-fluid -->
     </div>
